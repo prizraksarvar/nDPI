b707d7a9b938ce650afba915a94a621bb947e308
asdasdsdsadsada <sarkhas@mail.ru>
2021-04-18 01:46:37 +0300

    debug

--- src/lib/ndpi_main.c
+++ src/lib/ndpi_main.c
@@ -332,6 +332,56 @@ void ndpi_free(void *ptr)  {
 }
 #endif
 
+
+
+/* ****************************************************** */
+
+void ndpi_free_flow(struct ndpi_flow_struct *flow) {
+    if(flow) {
+        if(flow->http.url)
+            ndpi_free(flow->http.url);
+        if(flow->http.content_type)
+            ndpi_free(flow->http.content_type);
+        if(flow->http.user_agent)
+            ndpi_free(flow->http.user_agent);
+        if(flow->kerberos_buf.pktbuf)
+            ndpi_free(flow->kerberos_buf.pktbuf);
+
+        if(flow_is_proto(flow, NDPI_PROTOCOL_TLS) ||
+           flow_is_proto(flow, NDPI_PROTOCOL_QUIC)) {
+            if(flow->protos.stun_ssl.ssl.server_names)
+                ndpi_free(flow->protos.stun_ssl.ssl.server_names);
+
+            if(flow->protos.stun_ssl.ssl.alpn)
+                ndpi_free(flow->protos.stun_ssl.ssl.alpn);
+
+            if(flow->protos.stun_ssl.ssl.tls_supported_versions)
+                ndpi_free(flow->protos.stun_ssl.ssl.tls_supported_versions);
+
+            if(flow->protos.stun_ssl.ssl.issuerDN)
+                ndpi_free(flow->protos.stun_ssl.ssl.issuerDN);
+
+            if(flow->protos.stun_ssl.ssl.subjectDN)
+                ndpi_free(flow->protos.stun_ssl.ssl.subjectDN);
+
+            if(flow->l4.tcp.tls.srv_cert_fingerprint_ctx)
+                ndpi_free(flow->l4.tcp.tls.srv_cert_fingerprint_ctx);
+
+            if(flow->protos.stun_ssl.ssl.encrypted_sni.esni)
+                ndpi_free(flow->protos.stun_ssl.ssl.encrypted_sni.esni);
+        }
+
+        if(flow->l4_proto == IPPROTO_TCP) {
+            if(flow->l4.tcp.tls.message.buffer)
+                ndpi_free(flow->l4.tcp.tls.message.buffer);
+        }
+
+        ndpi_free(flow);
+    }
+}
+
+/* ****************************************************** */
+
 /* ****************************************** */
 
 void ndpi_flow_free(void *ptr) {
@@ -6569,54 +6619,6 @@ int ndpi_match_bigram(struct ndpi_detection_module_struct *ndpi_str,
 
   return(rc ? match.number : 0);
 }
-
-/* ****************************************************** */
-
-void ndpi_free_flow(struct ndpi_flow_struct *flow) {
-  if(flow) {
-    if(flow->http.url)
-      ndpi_free(flow->http.url);
-    if(flow->http.content_type)
-      ndpi_free(flow->http.content_type);
-    if(flow->http.user_agent)
-      ndpi_free(flow->http.user_agent);
-    if(flow->kerberos_buf.pktbuf)
-      ndpi_free(flow->kerberos_buf.pktbuf);
-
-    if(flow_is_proto(flow, NDPI_PROTOCOL_TLS) ||
-       flow_is_proto(flow, NDPI_PROTOCOL_QUIC)) {
-      if(flow->protos.stun_ssl.ssl.server_names)
-	ndpi_free(flow->protos.stun_ssl.ssl.server_names);
-
-      if(flow->protos.stun_ssl.ssl.alpn)
-	ndpi_free(flow->protos.stun_ssl.ssl.alpn);
-
-      if(flow->protos.stun_ssl.ssl.tls_supported_versions)
-	ndpi_free(flow->protos.stun_ssl.ssl.tls_supported_versions);
-
-      if(flow->protos.stun_ssl.ssl.issuerDN)
-	ndpi_free(flow->protos.stun_ssl.ssl.issuerDN);
-
-      if(flow->protos.stun_ssl.ssl.subjectDN)
-	ndpi_free(flow->protos.stun_ssl.ssl.subjectDN);
-
-      if(flow->l4.tcp.tls.srv_cert_fingerprint_ctx)
-	ndpi_free(flow->l4.tcp.tls.srv_cert_fingerprint_ctx);
-
-      if(flow->protos.stun_ssl.ssl.encrypted_sni.esni)
-	ndpi_free(flow->protos.stun_ssl.ssl.encrypted_sni.esni);
-    }
-
-    if(flow->l4_proto == IPPROTO_TCP) {
-      if(flow->l4.tcp.tls.message.buffer)
-	ndpi_free(flow->l4.tcp.tls.message.buffer);
-    }
-
-    ndpi_free(flow);
-  }
-}
-
-/* ****************************************************** */
 
 #ifndef __KERNEL__
 char *ndpi_revision() {
