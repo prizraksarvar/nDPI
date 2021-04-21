d133e4c3bf8e1da7775cc4a4d2bfef73d28f12c3
asdasdsdsadsada <sarkhas@mail.ru>
2021-04-21 22:36:39 +0300

    try extend port range for bittorrent to improve filter

--- src/lib/ndpi_main.c
+++ src/lib/ndpi_main.c
@@ -1271,7 +1271,7 @@ static void ndpi_init_protocol_defaults(struct ndpi_detection_module_struct *ndp
 			  ndpi_build_default_ports(ports_b, 0, 0, 0, 0, 0) /* UDP */);
   ndpi_set_proto_defaults(ndpi_str, 1 /* cleartext */, NDPI_PROTOCOL_ACCEPTABLE, NDPI_PROTOCOL_BITTORRENT,
 			  "BitTorrent", NDPI_PROTOCOL_CATEGORY_DOWNLOAD_FT,
-			  ndpi_build_default_ports(ports_a, 51413, 53646, 0, 0, 0) /* TCP */,
+			  ndpi_build_default_ports(ports_a, 51413, 65534, 0, 0, 0) /* TCP */,
 			  ndpi_build_default_ports(ports_b, 6771, 65534, 0, 0, 0) /* UDP */);
   ndpi_set_proto_defaults(ndpi_str, 0 /* encrypted */, NDPI_PROTOCOL_ACCEPTABLE, NDPI_PROTOCOL_SKYPE_TEAMS,
 			  "Skype_Teams", NDPI_PROTOCOL_CATEGORY_VOIP,
